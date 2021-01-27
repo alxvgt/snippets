83d71ff975f829891cab0397479250ef720c5b14
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2021-01-27 16:47:51 +0100

    Create about-host.sh
    initialize script to show host informations summary

---
+++ about-host.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# to use this file without git clone, use command below (you must be in root)
+# apt-get update && apt-get install -y wget && wget -O - https://raw.githubusercontent.com/alxvgt/snippets/master/about-host.sh --no-cache | bash
+
+clear
+printf "\n------------------- Host informations -----------------------\n"
+printf "\n##### Hostname\n"
+printf "\n```\n"
+hostname
+printf "\n```\n"
+printf "\n#### /etc/hosts\n"
+printf "\n```\n"
+cat /etc/hosts
+printf "\n```\n"
+printf "\n#### CPUs\n"
+printf "\n```\n"
+lscpu 
+printf "\n```\n"
+printf "\n#### Memory\n"
+printf "\n```\n"
+free -g 
+printf "\n```\n"
+printf "\n#### Hard disk\n"
+printf "\n```\n"
+df -h 
+printf "\n```\n"
+printf "\n#### OS\n" 
+printf "\n```\n"
+lsb_release -a
+printf "\n```\n"
