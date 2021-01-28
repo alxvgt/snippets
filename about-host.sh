b673bc5bbf6ad58efe007ff396b41b5d58eb5b7f
Alexandre Vingtier <1221867+alxvgt@users.noreply.github.com>
2021-01-28 09:14:21 +0100

    add apache / php informations

--- about-host.sh
+++ about-host.sh
@@ -4,6 +4,10 @@
 # if wget is not installed and you have permissions : apt-get update && apt-get install -y wget
 # wget -O - https://raw.githubusercontent.com/alxvgt/snippets/master/about-host.sh --no-cache | bash
 
+command_version () {
+  PATH=$PATH:/usr/sbin/ type $1 && PATH=$PATH:/usr/sbin/ $2 || printf "No command in path: $1\n"
+}
+
 clear
 printf "\n------------------- Host informations -----------------------\n"
 printf "\n##### Hostname\n"
@@ -30,3 +34,14 @@ printf "\n#### OS\n"
 printf "\n\`\`\`\n"
 lsb_release -a
 printf "\n\`\`\`\n"
+printf "\n#### Php\n" 
+printf "\n\`\`\`\n"
+command_version "php" "php -v"
+printf "\n\`\`\`\n"
+printf "\n#### Apache\n" 
+printf "\n\`\`\`\n"
+command_version "apachectl" "apachectl -v"
+printf "\n\`\`\`\n"
+
+
+
